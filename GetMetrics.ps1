a1de6c74f1d934fc6bdc5d55df4e900f60219b4a
phjrdla <30568443+phjrdla@users.noreply.github.com>
2018-06-19 17:29:16 +0200

    more metrics

--- GetMetrics.ps1
+++ GetMetrics.ps1
@@ -33,7 +33,7 @@ set trimspool on
 column "BEGINTIME"     format a19 trunc
 column "ENDTIME"       format a19 trunc
 column "LOAD"          format 099.999
-column "DBBCHNGPTXN"   format 099999
+column "DBBCHNGPTXN"   format 09999999
 column "REDOPSEC_MB"   format 0999.99
 column "REDOPTXN_MB"   format 0999.99
 column "HOSTCUPSEC"    format 099999.999
@@ -42,7 +42,11 @@ column "HOSTCUPCT"     format 099.999
 column "DBCPUTIMRATIO" format 0999.999
 column "AVGACTSESS"    format 0999
 column "SESSCNT"       format 0999
+
 column "EXECPSEC"      format 9999
+column "EXECPTXN"      format 9999
+column "EXECPUSC"      format 9999
+
 column "RESPTPTXN"     format 099.999
 column "EXECPSEC"      format 09999
 column "USERTXNPSEC"   format 09999
@@ -50,7 +54,12 @@ column "SQLRESPT"      format 099.999
 column "PARSEPTXN"     format 09999
 column "SHRFREEPCT"    format 099.999
 
-select 'BEGINTIME,HOSTCUPSEC,HOSTCUPCT,CUPSEC,DBCPUTIMRATIO,AVGACTSESS,SESSCNT,LOAD,EXECPSEC,RESPTPTXN,SQLRESPT,USERCALLSPSEC,USERTXNPSEC,SHRFREEPCT,ENQRPTXN,DBBCHNGPTXN,REDOPSEC_MB,REDOPTXN_MB,PARSEPTXN' "metrics"
+column "USERCALLSPSEC" format 09999
+column "USERCALLSPTXN" format 09999
+column "USERTXNPSEC"   format 09999
+
+
+select 'BEGINTIME,HOSTCUPSEC,HOSTCUPCT,CUPSEC,DBCPUTIMRATIO,AVGACTSESS,SESSCNT,LOAD,EXECPSEC,EXECPTXN,EXECPUSERCALL,RESPTPTXN,SQLRESPT,USERCALLSPSEC,USERCALLSPTXN,USERTXNPSEC,SHRFREEPCT,ENQRPTXN,DBBCHNGPTXN,REDOPSEC_MB,REDOPTXN_MB,PARSEPTXN' "metrics"
   from dual
 /
 select to_char(begin_time, 'YYYY-MM-DD HH24:MI:SS')                                          "BEGINTIME", ','
@@ -62,9 +71,12 @@ select to_char(begin_time, 'YYYY-MM-DD HH24:MI:SS')                             
       ,round( sum ( decode( metric_name, 'Session Count', value) ) )                         "SESSCNT", ','
 	  ,round( sum ( decode( metric_name, 'Current OS Load', value) ) )                       "LOAD", ','
 	  ,round( sum ( decode( metric_name, 'Executions Per Sec', value) ) )                    "EXECPSEC", ','
+      ,round( sum ( decode( metric_name, 'Executions Per Txn', value) ) )                    "EXECPTXN", ','
+      ,round( sum ( decode( metric_name, 'Executions Per User Call', value) ) )              "EXECPUSERCALL", ','
 	  ,round( sum ( decode( metric_name, 'Response Time Per Txn', value/100) ), 3 )          "RESPTPTXN" , ','
 	  ,round( sum ( decode( metric_name, 'SQL Service Response Time', value/100) ), 3 )      "SQLRESPT" , ','
 	  ,round( sum ( decode( metric_name, 'User Calls Per Sec', value) ) )                    "USERCALLSPSEC" , ','
+      ,round( sum ( decode( metric_name, 'User Calls Per Txn', value) ) )                    "USERCALLSPTXN" , ','
 	  ,round( sum ( decode( metric_name, 'User Transaction Per Sec', value) ) )              "USERTXNPSEC" , ','
 	  ,round( sum ( decode( metric_name, 'Shared Pool Free %', value) ), 3 )                 "SHRFREEPCT" , ','
 	  ,round( sum ( decode( metric_name, 'Enqueue Requests Per Txn', value) ) )              "ENQRPTXN" , ','
